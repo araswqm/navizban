8d4066e35dfd1e4d99c4b0b1eb99eadc3643a148
araswqm <arasatal1634@gmail.com>
2026-07-10 20:46:22 +0300

    fix alias

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,17 +1,1 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
-
-const config = getDefaultConfig(__dirname);
-
-config.watchFolders = [
-  path.resolve(__dirname, "../../lib"),
-];
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, "node_modules"),
-  path.resolve(__dirname, "../../node_modules"),
-];
-
-// pnpm symlink yapısı için Metro'nun symlink'leri takip etmesi gerekir
-config.resolver.resolveSymlinks = true;
-
-module.exports = config;
+const { getDefaultConfig } = require("expo/metro-config"); const path = require("path"); const config = getDefaultConfig(__dirname); config.watchFolders = [path.resolve(__dirname, "../../lib")]; config.resolver.nodeModulesPaths = [path.resolve(__dirname, "node_modules"), path.resolve(__dirname, "../../node_modules")]; config.resolver.extraNodeModules = { "@": path.resolve(__dirname) }; config.resolver.resolveSymlinks = true; module.exports = config;
